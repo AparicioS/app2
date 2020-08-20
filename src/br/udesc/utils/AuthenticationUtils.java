d73b8d207a978e672282d78470c6b018922719a2
Luciano velho <lucianovelgarcia@gmail.com>
2020-08-20 17:43:32 -0300

    Classes Para autorização

---
+++ src/br/udesc/utils/AuthenticationUtils.java
@@ -0,0 +1,109 @@
+package br.udesc.utils;
+
+import java.lang.reflect.Method;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.container.ResourceInfo;
+
+import br.udesc.modelo.Usuario;
+
+public class AuthenticationUtils {
+
+	private static ResourceInfo _resourceInfo;
+
+	
+	private static Map< String, Usuario> usuarios = new HashMap<>();
+	
+	static {
+		 
+		usuarios.put("a", new Usuario("a", "a")); // YTph
+//		usuarios.put("b", new Usuario("b", "b", "inserirCliente")); // Yjpi
+//		Usuario u = new Usuario("c", "c", "admin");
+//		u.addRole("alterarCliente");
+//		usuarios.put("c", u); // Yzpj
+		
+	}
+	
+	public static boolean isUsuarioAutenticado(String authString){
+        
+		if (authString == null)
+			return false;
+		
+        try {
+            String decodedAuth = "";
+            // Header is in the format "Basic 5tyc0uiDat4"
+            // We need to extract data before decoding it back to original string
+            String[] authParts = authString.split("\\s+");
+            String authInfo = authParts[1];
+            // Decode the data back to original string
+            byte[] bytes = null;
+            bytes = Base64.getDecoder().decode(authInfo);
+            decodedAuth = new String(bytes);
+            
+	        StringTokenizer tokenizer = new StringTokenizer(decodedAuth, ":");
+	        String username = tokenizer.nextToken();
+	        String password = tokenizer.nextToken();
+        
+	        String[] roles;
+	        if (_resourceInfo != null) {
+	        	
+	        	Method method = _resourceInfo.getResourceMethod();
+	        	RolesAllowed[] anotacoes = method.getAnnotationsByType(RolesAllowed.class);
+	        	if (anotacoes.length == 1) {
+	        		roles = anotacoes[0].value();
+	        	} else {
+	        		roles = new String[] {};
+	        	}
+	        	
+	        } else {
+	        	roles = new String[] {};
+	        }
+	        
+	        /**
+	         * aqui voce vai incluir a sua logica para validar o usuario e a senha.
+	         * Por exemplo, puxar do BD. Eu estou usando Map soh para exemplificar.
+	         */
+	        
+	        Usuario usuario = usuarios.get(username);
+	        
+	        if ( usuario != null && usuario.getSenha().equals(password)) {
+	        
+	        	if (roles.length > 0) {
+		        	
+		        	for (String role:roles) {
+//		        		if (usuario.hasRole(role)) 
+		        			return true;
+		        	}
+		        	
+		        	return false;
+	        	}
+	        	
+	            return true;
+	        } else {
+	        	return false;
+	        }
+        } catch (Exception e) {
+        	return false;
+        }
+         
+    }
+
+	public static boolean isMetodoPermitido() {
+		
+		Method method = _resourceInfo.getResourceMethod();
+    	RolesAllowed[] anotacoes = method.getAnnotationsByType(RolesAllowed.class);
+    	
+		return anotacoes.length == 0;
+	}
+	
+	public static void setResourceInfo(ResourceInfo resourceInfo) {
+		
+		_resourceInfo = resourceInfo;
+		
+	}
+
+}
